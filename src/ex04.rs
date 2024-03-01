562e07a0c83f828a9366565c8b0f4f76dadec426
sirkotal <lordkotal2002@gmail.com>
2024-03-01 10:55:50 +0000

    Finished optimizing exercise 4 (passes the final test)

--- src/ex04.rs
+++ src/ex04.rs
@@ -1,21 +1,23 @@
 pub fn isPrime(n: u32) -> bool {
-    let mut divs = 1;
-    for i in 1..((n/2)+1) {
-        if (n % i == 0) {
-            divs += 1;
+    if (n < 2) {
+        return false;
+    }
+    let mut d = 2;
+
+    while (d < ((n/2)+1)) {
+        if (n % d == 0) {
+            return false;
         }
+        d += 1;
     }
 
-    if (divs == 2) {
-        return true;
-    }
-    return false;
+    return true;
 }
 
 pub fn nth(n: u32) -> u32 {
     let mut count = 0;
-    let mut start = 2;
-    let mut prime = 0;
+    let mut start = 3;
+    let mut prime = 2;
 
     while (count != n) {
         if (isPrime(start)) {
