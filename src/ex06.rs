0450897b2e6d620629350cbde5477b7f9f1083af
sirkotal <lordkotal2002@gmail.com>
2024-03-02 13:13:47 +0000

    Added a more optimal - O(1) - version of exercise 6

--- src/ex06.rs
+++ src/ex06.rs
@@ -19,3 +19,19 @@ pub fn sum_of_squares(n: u32) -> u32 {
 pub fn difference(n: u32) -> u32 {
     return (square_of_sum(n) - sum_of_squares(n));
 }
+
+pub fn square_of_sum_alt(n: u32) -> u32 {
+    let mut res = (n * (n + 1)) / 2;
+
+    return u32::pow(res, 2);
+}
+
+pub fn sum_of_squares_alt(n: u32) -> u32 {
+    let mut res = (n * (n + 1) * ((2*n) + 1)) / 6;
+
+    return res;
+}
+
+pub fn difference_alt(n: u32) -> u32 {
+    return (square_of_sum_alt(n) - sum_of_squares_alt(n));
+}
